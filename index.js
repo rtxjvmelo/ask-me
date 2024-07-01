d1adf9cbd277f59eb9502008e3344fabfe9bd071
rtxjvmelo <jmelo3259@gmail.com>
2024-07-01 08:44:09 -0300

    implementação da rota de salvar a pergunta

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
 const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+
 app.get('/', (req, res) =>{
     res.render('index')
 })
 
-
 app.get("/perguntar", (req,res) =>{
     res.render('perguntar')
+})
+
+app.post("/salvarpergunta", (req, res) =>{
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send(`Titulo: ${titulo} / Descrição: ${descricao}`)
 })
 
 app.listen(port, (erro) =>{
